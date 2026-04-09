6d7d378d04b5d289c21e93c855e491d5f300c303
gitcoder-amit <yadavamit14801@gmail.com>
2026-04-09 19:04:44 +0530

    added view for pdf summarizer

--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import PDFSerializer
 from .models import PDFUploader
+from .utils import summarize_pdf
+from django.conf import settings
 
 # Create your views here.
 
@@ -15,5 +17,9 @@ class PdfSummarizerAPI(APIView):
         serializer = PDFSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            actual_path = f"{settings.BASE_DIR}/pdfs/{serializer.validated_data['pdf_file'].name}"
+            print('$$$$$$$$$$$$$$$')
+            print(actual_path)
+            data = summarize_pdf(actual_path)
+            return Response({"summary": data}, status=201)
         return Response(serializer.errors, status=400)
