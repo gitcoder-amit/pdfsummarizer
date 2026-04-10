ed1b62e387bd1e335ce4265ae1aef3b1634f9d34
gitcoder-amit <yadavamit14801@gmail.com>
2026-04-10 19:09:52 +0530

    added Playwright to convert text to pdf

--- home/utils.py
+++ home/utils.py
@@ -2,6 +2,11 @@ import pdfplumber
 import requests
 from django.conf import settings
 import os
+from django.template.loader import render_to_string
+import pdfkit
+from playwright.sync_api import sync_playwright
+
+
 
 ENDPOINT = os.getenv("GROQ_ENDPOINT")
 GROQ_API = os.getenv("GROQ_API")
@@ -21,12 +26,28 @@ def call_llm_api(text):
             {"role": "user", "content": prompt}
         ],
         "temperature": 0.7,
-        "max_tokens": 150
+        # "max_tokens": 150
     }
 
     response = requests.post(ENDPOINT, json=payload, headers=headers)
     response.raise_for_status()
     return response.json()['choices'][0]['message']['content'].strip()
+
+def convert_to_pdf(text):
+    try:
+        html = render_to_string('pdf_template.html', {'content': text})
+        output_dir = os.path.join(settings.BASE_DIR, 'outputpdfs')
+        os.makedirs(output_dir, exist_ok=True)
+        output_path = os.path.join(output_dir, 'summary.pdf')
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            page.set_content(html)
+            page.pdf(path=output_path)
+            browser.close()
+    except Exception as e:
+        print(f"Error generating PDF: {e}")
+        return None
 
 def read_pdf(file_path):
     try:
@@ -38,11 +59,11 @@ def read_pdf(file_path):
     except Exception as e:
         print(f"Error reading PDF: {e}")
         return None
-    s
+    
     
 def summarize_pdf(file_path, max_length=100):
     text = read_pdf(file_path)
     if text:
         summarized_text  = call_llm_api(text)
-        return summarized_text
+        return convert_to_pdf(summarized_text)
     return None
